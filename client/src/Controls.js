ee756d4525f2c489b035d5e66afcb5cbd440bc0f
R-J <kgerbil@gmail.com>
2021-01-18 11:30:05 -0800

    Controls styles update

--- client/src/Controls.js
+++ client/src/Controls.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useCallback, useEffect, useState, useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Fade from '@material-ui/core/Fade';
 import Grid from '@material-ui/core/Grid';
@@ -6,32 +6,48 @@ import Paper from '@material-ui/core/Paper';
 import PauseIcon from '@material-ui/icons/Pause';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import Typography from '@material-ui/core/Typography';
 
 const useControlStyles = makeStyles((theme) => ({
     container: {
         position: 'absolute',
         left: '50%',
-        width: '98%',
-        bottom: '3%'
+        width: '100%',
+        bottom: 0
+    },
+    buttonContainer: {
+        flexDirection: 'row'
+    },
+    timeDisplay: {
+        color: 'white',
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: '100%',
+        marginLeft: 10
     },
     paper: {
-        background: 'black',
+        background: "linear-gradient(to bottom, rgba(0,0,0,0), rgba(96,96,96,1))",
         position: 'relative',
         left: '-50%',
         zIndex: 10,
-        flexGrow: 1
+        padding: 10
+    },
+    playButtonContainer: {
+        display: 'flex',
+        flexDirection: "column",
+        justifyContent: 'center',
+        height: "100%",
     },
     playButton: {
         color: 'white',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 30
+        width: 35,
+        height: 30,
+        marginBottom: 0,
+        cursor: 'pointer'
     },
     progress: {
-        top: '40%',
-        height: '20%',
-        marginRight: 10,
+        margin: 5
     },
     bar1Determinate: {
         transition: 'none',
@@ -39,18 +55,85 @@ const useControlStyles = makeStyles((theme) => ({
     }
 }));
 
+const useProgressBarStyles = makeStyles((theme) => ({
+    container: {
+        width: '100%',
+        background: 'rgba(0,0,0,0.5)',
+        height: 5,
+        marginBottom: 5,
+        cursor: 'pointer'
+    },
+    fillContainer: {
+        width: '100%'
+    },
+    fill: {
+        background: 'linear-gradient(to left, #e21e4a, #a92fff)',
+        height: '100%'
+    },
+    handleContainer: {
+        position: 'relative',
+        width: 0,
+        height: 0
+    },
+    handle: {
+        borderRadius: '50%',
+        width: 15,
+        height: 15,
+        background: 'white',
+        position: 'absolute',
+        top: 5,
+        left: 2,
+        filter: 'drop-shadow(10px 10px 10px #cccccc)'
+    }
+}));
+
+function displayTime(milliseconds) {
+    const seconds = Math.floor(milliseconds / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const secondsInMinute = seconds % 60;
+    return String(minutes) + ':' + String(secondsInMinute).padStart(2, '0');
+}
+
+function elementWidth(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.right - rect.left;
+}
+
+function ProgressBar(props) {
+    const classes = useProgressBarStyles();
+    const containerRef = useRef(null);
+
+    function handleClick(e) {
+        const rect = e.currentTarget.getBoundingClientRect();
+        const width = rect.right - rect.left;
+        const progress = (e.pageX - rect.left) / width;
+
+        props.onSeek(progress);
+    };
+
+    const fillStyle = {width: props.value + '%'};
+    const handleStyle = {marginLeft: containerRef.current ? elementWidth(containerRef.current) * props.value / 100 : 0};
+
+    return (
+        <div ref={containerRef} onClick={handleClick} className={classes.container}>
+            <div className={classes.fill} style={fillStyle}></div>
+            <div className={classes.handleAnchor}>
+                <div className={classes.handle} style={handleStyle}></div>
+            </div>
+        </div>
+    );
+}
+
 export default function Controls(props) {
     const classes = useControlStyles();
     const [show, setShow] = useState(true);
     const lastMousePositionRef = useRef({x: 0, y: 0});
     const lastShowTimestampRef = useRef(Date.now());
     const lastShowRef = useRef(true);
+    const [, updateState] = useState();
+    const forceUpdate = useCallback(() => updateState({}), []);
 
-    function handleSeek(e) {
-        const rect = e.currentTarget.getBoundingClientRect();
-        const width = rect.right - rect.left;
-        const progress = (e.pageX - rect.left) / width;
-
+    function handleSeek(progress) {
         props.onSeek(progress);
     };
 
@@ -94,28 +177,38 @@ export default function Controls(props) {
         };
     }, [props]);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            forceUpdate()
+        }, 100);
+
+        return () => clearInterval(interval);
+    }, [forceUpdate]);
+
     const progress = props.clock.progress(props.length);
 
     return (
         <div className={classes.container}>
             <Fade in={show} timeout={200}>
-                <Paper square className={classes.paper}>
+                <div className={classes.paper}>
+                    <ProgressBar
+                        onSeek={handleSeek}
+                        value={progress * 100} />
                     <Grid container direction="row">
                         <Grid item>
+                            <div className={classes.playButtonContainer}>
                             {props.playing
                                 ? <PauseIcon onClick={props.onPause} className={classes.playButton} />
                                 : <PlayArrowIcon onClick={props.onPlay} className={classes.playButton} />}
+                            </div>
                         </Grid>
-                        <Grid item xs>
-                            <LinearProgress
-                                onClick={handleSeek}
-                                classes={{bar1Determinate: classes.bar1Determinate}}
-                                className={classes.progress}
-                                variant="determinate"
-                                value={progress * 100} />
+                        <Grid item>
+                            <Typography variant="h6" className={classes.timeDisplay}>
+                                {displayTime(progress * props.length)}
+                            </Typography>
                         </Grid>
                     </Grid>
-                </Paper>
+                </div>
             </Fade>
         </div>
     );
