f7ae611e61cc04e0abf8f1b717ae033e30961f36
R-J <kgerbil@gmail.com>
2021-01-23 10:00:29 -0800

    Copy history UX minor improvements
    - List items don't auto focus. Before, the auto-focus would prevent
      copying text out of yomi-chan popups.
    - List scrolls to bottom automatically when opened.

--- client/src/CopyHistory.js
+++ client/src/CopyHistory.js
@@ -1,21 +1,39 @@
+import { useCallback } from 'react';
 import Popover from '@material-ui/core/Popover';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListSubheader from '@material-ui/core/ListSubheader';
 
+
 export default function CopyHistory(props) {
+    const scrollToBottomRefCallback =  useCallback(element => {
+        if (element) {
+            element.scrollIntoView();
+        }
+    }, []);
+
     let content = null;
 
     if (props.items.length > 0) {
         content = [];
         let lastSeenItemName = null;
+        let i = 0;
+
         for (const item of props.items) {
             if (lastSeenItemName === null || lastSeenItemName !== item.name) {
                 lastSeenItemName = item.name;
                 content.push((<ListSubheader disableSticky={true} key={item.name}>{item.name}</ListSubheader>));
             }
-            content.push((<ListItem key={item.timestamp}>{item.text}</ListItem>));
+
+            if (i === props.items.length - 1) {
+                content.push((<ListItem ref={scrollToBottomRefCallback} key={item.timestamp}>{item.text}</ListItem>));
+            } else {
+                content.push((<ListItem key={item.timestamp}>{item.text}</ListItem>));
+            }
+
+            ++i;
         }
+
     } else {
         content = (<ListItem>Copy history is empty.</ListItem>);
     }
@@ -24,6 +42,7 @@ export default function CopyHistory(props) {
 
     return (
         <Popover
+            disableEnforceFocus={true}
             open={props.open}
             anchorEl={props.anchorEl}
             onClose={props.onClose}
