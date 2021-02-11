69417d403e58e3fa8fe5111e3da8fac92f89d72c
R-J <kgerbil@gmail.com>
2021-02-10 19:23:59 -0800

    Delete java app

--- src/main/java/org/gerbil/asbplayer/FileNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.gerbil.asbplayer;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "file not found")
-public class FileNotFoundException extends RuntimeException {
-}
--- src/main/java/org/gerbil/asbplayer/FileStore.java
+++
@@ -1,8 +0,0 @@
-package org.gerbil.asbplayer;
-
-import org.springframework.content.commons.repository.Store;
-import org.springframework.content.rest.StoreRestResource;
-
-@StoreRestResource(path="stream")
-public interface FileStore extends Store<String> {
-}
