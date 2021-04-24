04476359d920af2217e42ad6c148a8279cfdde2d
R-J <kgerbil@gmail.com>
2021-04-24 11:46:51 -0700

    Submit issue button

--- client/src/components/Bar.js
+++ client/src/components/Bar.js
@@ -1,6 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import AppBar from '@material-ui/core/AppBar';
+import BugReportIcon from '@material-ui/icons/BugReport';
 import HelpIcon from '@material-ui/icons/Help';
 import IconButton from '@material-ui/core/IconButton';
 import ListIcon from '@material-ui/icons/List';
@@ -63,6 +64,17 @@ export default function Bar(props) {
                 <Typography variant="h6" className={classes.title}>
                     {props.title}
                 </Typography>
+                <Tooltip title="Submit Issue">
+                    <IconButton
+                        edge="end"
+                        component="a"
+                        href="https://github.com/killergerbah/asbplayer/issues"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <BugReportIcon />
+                    </IconButton>
+                </Tooltip>
                 <Tooltip title="Help">
                     <IconButton
                         edge="end"
