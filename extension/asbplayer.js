8251a7d8f1e2cca2155635664f97d42b7585baa4
R-J <kgerbil@gmail.com>
2021-05-19 18:16:16 -0700

    Extension version 0.7.0

--- extension/asbplayer.js
+++ extension/asbplayer.js
@@ -25,7 +25,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
         sender: 'asbplayer-extension-to-player',
         message: {
             command: 'version',
-            version: '0.6.2'
+            version: '0.7.0'
         }
     });
 });
