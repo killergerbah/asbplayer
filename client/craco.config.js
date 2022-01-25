10d6c9da22c3974339eb3db6e60cd8d9e039411c
R-J <kgerbil@gmail.com>
2022-01-25 08:51:56 -0800

    Fix client's babel include rule
    - Make it more specific
    - Fixes mp3 encoder not working when using development server
    - Fixes prod build not working

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
+
 // create-react-app scripts don't work with typescript projects linked by yarn out-of-the-box
 // https://stackoverflow.com/questions/65893787/create-react-app-with-typescript-and-npm-link-enums-causing-module-parse-failed
 module.exports = {
@@ -12,7 +17,10 @@ module.exports = {
                         ...rule,
                         oneOf: rule.oneOf.map((ruleObject) => {
                             if (!new RegExp(ruleObject.test).test('.ts') || !ruleObject.include) return ruleObject;
-                            return { ...ruleObject, include: undefined };
+                            return {
+                                ...ruleObject,
+                                include: [resolveApp('src'), resolveApp('../common')],
+                            };
                         }),
                     };
                 }),
