c83ced66d98e3b1ed1557be5824c00a714df0532
R-J <kgerbil@gmail.com>
2022-02-20 08:41:20 -0800

    SubtitleReader in typescript

--- client/src/services/SubtitleReader.ts
+++ client/src/services/SubtitleReader.ts
@@ -5,25 +5,32 @@ import { WebVTT } from 'vtt.js';
 const tagRegex = RegExp('</?([^>]*)>', 'ig');
 const helperElement = document.createElement('div');
 
+interface SubtitleNode {
+    start: number;
+    end: number;
+    text: string;
+    track: number;
+}
+
 export default class SubtitleReader {
-    async subtitles(files) {
+    async subtitles(files: File[]) {
         return (await Promise.all(files.map((f, i) => this._subtitles(f, i))))
             .flatMap((nodes) => nodes)
             .sort((n1, n2) => n1.start - n2.start);
     }
 
-    async _subtitles(file, track) {
+    async _subtitles(file: File, track: number): Promise<SubtitleNode[]> {
         if (file.name.endsWith('.srt')) {
             const nodes = parseSrt(await file.text());
-            return nodes.map((node) => ({ ...node.data, track: track }));
+            return nodes.map((node: any) => ({ ...node.data, track: track }));
         }
 
         if (file.name.endsWith('.vtt') || file.name.endsWith('.nfvtt')) {
             return new Promise(async (resolve, reject) => {
                 const isFromNetflix = file.name.endsWith('.nfvtt');
                 const parser = new WebVTT.Parser(window, WebVTT.StringDecoder());
-                const cues = [];
-                parser.oncue = (c) => {
+                const cues: any[] = [];
+                parser.oncue = (c: any) => {
                     if (isFromNetflix) {
                         c.text = c.text.replace(tagRegex, '');
 
@@ -43,7 +50,7 @@ export default class SubtitleReader {
                         cues.map((c) => ({
                             start: Math.floor(c.startTime * 1000),
                             end: Math.floor(c.endTime * 1000),
-                            text: c.text,
+                            text: c.text as string,
                             track: track,
                         }))
                     );
@@ -69,12 +76,12 @@ export default class SubtitleReader {
 
             for (let index = 0, length = textNodes.length; index < length; index++) {
                 const elm = textNodes[index];
-                const start = parseFloat(elm.getAttribute('start'));
+                const start = parseFloat(elm.getAttribute('start') as string);
 
                 subtitles.push({
                     start: Math.floor(start * 1000),
-                    end: Math.floor((start + parseFloat(elm.getAttribute('dur'))) * 1000),
-                    text: this._decodeHTML(elm.textContent.replace(tagRegex, '')),
+                    end: Math.floor((start + parseFloat(elm.getAttribute('dur') as string)) * 1000),
+                    text: this._decodeHTML(elm.textContent!.replace(tagRegex, '')),
                     track,
                 });
             }
@@ -85,7 +92,7 @@ export default class SubtitleReader {
         throw new Error('Unsupported subtitle file format');
     }
 
-    _fixRTL(line) {
+    _fixRTL(line: string): string {
         const index1 = line.indexOf('&lrm;');
         const index2 = line.indexOf('&rlm;');
         let newLine = '';
@@ -101,8 +108,8 @@ export default class SubtitleReader {
         return line;
     }
 
-    _decodeHTML(text) {
+    _decodeHTML(text: string): string {
         helperElement.innerHTML = text;
-        return helperElement.childNodes.length === 0 ? '' : helperElement.childNodes[0].nodeValue;
+        return helperElement.childNodes.length === 0 ? '' : helperElement.childNodes[0].nodeValue!;
     }
 }
