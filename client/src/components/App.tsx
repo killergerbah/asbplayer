62a6dddd1b19f18e66de7528154d86f4436044b8
R-J <kgerbil@gmail.com>
2022-05-31 18:15:21 -0700

    Extension version 0.18.2

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -38,7 +38,7 @@ import VideoPlayer from './VideoPlayer';
 import { Color } from '@material-ui/lab';
 import { AnkiExportMode } from '@project/common';
 
-const latestExtensionVersion = '0.18.1';
+const latestExtensionVersion = '0.18.2';
 const extensionUrl = 'https://github.com/killergerbah/asbplayer/releases/latest';
 
 const useContentStyles = makeStyles<Theme, ContentProps>((theme) => ({
