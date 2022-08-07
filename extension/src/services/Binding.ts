130908530f74ff67f0c70026a3afa7cb3178bf51
R-J <kgerbil@gmail.com>
2022-08-07 09:25:39 -0700

    Remove UI priming when mining subtitle due to perf

--- extension/src/services/Binding.ts
+++ extension/src/services/Binding.ts
@@ -390,10 +390,6 @@ export default class Binding {
                             } else {
                                 this._toggleRecordingMedia(copySubtitleMessage.postMineAction);
                             }
-
-                            if (copySubtitleMessage.postMineAction === PostMineAction.showAnkiDialog) {
-                                this.ankiUiContainer.prime(this);
-                            }
                         }
                         break;
                     case 'card-updated':
