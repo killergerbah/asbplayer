a4f9418391840faed4a245b026bb58594c917a4a
R-J Lim <rj@n3twork.com>
2023-01-11 08:23:59 -0800

    Extract video name even if subs are not provided on bandai channel

--- extension/src/pages/bandai-channel-page.js
+++ extension/src/pages/bandai-channel-page.js
@@ -30,19 +30,47 @@ setTimeout(() => {
                 }
             }
 
-            if (typeof value?.name === 'string') {
-                basename = value?.name;
+            if (typeof value.bc.name === 'string') {
+                basename = value.bc.name;
             }
+        }
+
+        if (basename === undefined && typeof value?.bch?.episode_title === 'string') {
+            basename = value.bch.episode_title
         }
 
         return value;
     };
 }, 0);
 
+function basenameFromDOM() {
+    const seriesElement = document.getElementById('bch-series-title');
+    const episodeElement = document.getElementById('bch-story-title');
+
+    if (!seriesElement || !episodeElement) {
+        return undefined;
+    }
+
+    if (!seriesElement.textContent) {
+        return undefined;
+    }
+
+    if (!episodeElement.childNodes || episodeElement.childNodes.length === 0) {
+        return undefined;
+    }
+
+    return `${seriesElement.textContent} ${episodeElement.childNodes[0].nodeValue}`;
+}
+
 document.addEventListener(
     'asbplayer-get-synced-data',
     () => {
-        const response = { error: '', basename: basename ?? document.title, extension: 'vtt', subtitles: subtitles };
+        const response = {
+            error: '',
+            basename: basename ?? basenameFromDOM() ?? document.title,
+            extension: 'vtt',
+            subtitles: subtitles,
+        };
         document.dispatchEvent(
             new CustomEvent('asbplayer-synced-data', {
                 detail: response,
