9d9eb95577c00e37c8a53d5acf2408fde3752505
R-J Lim <rj@n3twork.com>
2023-05-31 19:47:18 -0700

    Fix extension not being detected by app sometimes

--- client/src/hooks/use-chrome-extension.ts
+++ client/src/hooks/use-chrome-extension.ts
@@ -9,38 +9,50 @@ const keyBindNameMap: any = {
     'take-screenshot': 'takeScreenshot',
 };
 
+const initialExtension = new ChromeExtension();
+let realExtension: ChromeExtension | undefined;
+
+const listenForVersion = (callback: (extension: ChromeExtension) => void) => {
+    const versionListener = (event: MessageEvent) => {
+        if (event.source !== window) {
+            return;
+        }
+
+        if (event.data.sender === 'asbplayer-extension-to-player') {
+            if (event.data.message.command === 'version') {
+                const message = event.data.message as ExtensionVersionMessage;
+                const translatedCommands: { [key: string]: string | undefined } = {};
+                const extensionCommands = message.extensionCommands ?? {};
+
+                for (const extensionCommandName of Object.keys(extensionCommands)) {
+                    translatedCommands[keyBindNameMap[extensionCommandName]] = extensionCommands[extensionCommandName];
+                }
+
+                callback(new ChromeExtension(message.version, translatedCommands));
+            }
+        }
+    };
+
+    window.addEventListener('message', versionListener);
+
+    return () => {
+        window.removeEventListener('message', versionListener);
+    };
+};
+
+const unbindInitialListener = listenForVersion((extension) => (realExtension = extension));
+
 export const useChromeExtension = () => {
-    const [extension, setExtension] = useState<ChromeExtension>(new ChromeExtension());
+    const [extension, setExtension] = useState<ChromeExtension>(initialExtension);
 
     useEffect(() => {
-        const listener = (event: MessageEvent) => {
-            if (event.source !== window) {
-                return;
-            }
-
-            if (event.data.sender === 'asbplayer-extension-to-player') {
-                if (event.data.message.command === 'version') {
-                    const message = event.data.message as ExtensionVersionMessage;
-                    const translatedCommands: { [key: string]: string | undefined } = {};
-                    const extensionCommands = message.extensionCommands ?? {};
-
-                    for (const extensionCommandName of Object.keys(extensionCommands)) {
-                        translatedCommands[keyBindNameMap[extensionCommandName]] =
-                            extensionCommands[extensionCommandName];
-                    }
-
-                    setExtension(new ChromeExtension(message.version, translatedCommands));
-                }
-            }
-        };
-
-        window.addEventListener('message', listener);
-
-        return () => {
-            extension.unbind();
-            window.removeEventListener('message', listener);
-        };
-    }, [extension]);
+        if (realExtension) {
+            setExtension(realExtension);
+        } else {
+            unbindInitialListener();
+            return listenForVersion(setExtension);
+        }
+    }, []);
 
     return extension;
 };
