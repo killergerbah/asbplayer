eae25a3d2a198384c3713844cfa6fa9986b92e94
R-J Lim <kgerbil@gmail.com>
2023-11-27 12:13:43 +0900

    Add and configure eslint
    - The website workspace already has eslint but files in the common
      workspace are not included in the linting
    - extension and common workspaces need to be linted anyway,
      motivating inclusion of eslint at root of project

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+const reactRecommended = require('eslint-plugin-react/configs/recommended');
+const reactHooks = require('eslint-plugin-react-hooks');
+const typescriptParser = require('@typescript-eslint/parser');
+const globals = require('globals');
+
+const globalsBrowser = { ...globals.browser };
+
+if (!('AudioWorkletGlobalScope' in globalsBrowser)) {
+    // This particular key in the globals object has a trailing space, so for now we work around that problem here
+    globalsBrowser['AudioWorkletGlobalScope'] = globalsBrowser['AudioWorkletGlobalScope '];
+    delete globalsBrowser['AudioWorkletGlobalScope '];
+}
+
+module.exports = [
+    {
+        files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+        ...reactRecommended,
+        rules: {
+            ...reactRecommended.rules,
+            ...reactHooks.configs.recommended.rules,
+        },
+        plugins: {
+            ...reactRecommended.plugins,
+            'react-hooks': { rules: { ...reactHooks.rules } },
+        },
+        settings: {
+            ...reactRecommended.settings,
+            react: {
+                // ...reactRecommended.settings.react,
+                version: 'detect',
+            },
+        },
+        languageOptions: {
+            ...reactRecommended.languageOptions,
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            parser: typescriptParser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: {
+                ...globals.serviceworker,
+                ...globalsBrowser,
+            },
+        },
+    },
+];
