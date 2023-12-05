732529840eca34390ad6e7df416af2c5d09804e4
R-J Lim <kgerbil@gmail.com>
2023-12-05 16:34:32 +0900

    Do not copy .DS_Store to extension build output

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -64,7 +64,7 @@ module.exports = (env, options) => ({
                 {
                     from: './src',
                     globOptions: {
-                        ignore: ['**/services', '**/handlers', '**/ui'],
+                        ignore: ['**/services', '**/handlers', '**/ui', '**/.DS_Store'],
                     },
                 },
                 {
