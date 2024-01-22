f5d1d2a7dad0b74673a77bb960959a28720ff7c6
R-J Lim <kgerbil@gmail.com>
2024-01-22 13:12:02 +0900

    Restore new 'seek to beginning of curr/prev subtitle' loc key
    - But keep the old key as well to maintain backwards compatibility
    - This change updates the label for the key bind as desired for new
      versions of asbplayer while maintaining the old label for old
      versions.

--- common/components/SettingsForm.tsx
+++ common/components/SettingsForm.tsx
@@ -615,7 +615,7 @@ export default function SettingsForm({
             seekToPreviousSubtitle: { label: t('binds.seekToPreviousSubtitle')!, boundViaChrome: false },
             seekToNextSubtitle: { label: t('binds.seekToNextSubtitle')!, boundViaChrome: false },
             seekToBeginningOfCurrentSubtitle: {
-                label: t('binds.seekToBeginningOfCurrentSubtitle')!,
+                label: t('binds.seekToBeginningOfCurrentOrPreviousSubtitle')!,
                 boundViaChrome: false,
             },
             adjustOffsetToPreviousSubtitle: {
