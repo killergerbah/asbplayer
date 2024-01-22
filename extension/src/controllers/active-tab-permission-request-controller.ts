fa3c549eca57b327f625ff9da25cc2ab5ed36de7
R-J Lim <kgerbil@gmail.com>
2024-01-22 09:29:52 +0900

    Fix audio recoring permission dialog not responding when opened twice

--- extension/src/controllers/active-tab-permission-request-controller.ts
+++ extension/src/controllers/active-tab-permission-request-controller.ts
@@ -35,7 +35,6 @@ export default class ActiveTabPermissionRequestController {
     async show() {
         this._frame.language = await this._context.settings.getSingle('language');
         const isNewClient = await this._frame.bind();
-        this._client?.unbind();
         this._client = await this._frame.client();
 
         if (isNewClient) {
