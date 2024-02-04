32f4fa5aed4ba1f76dd19e2f43f6b43e39505eb5
Beangate <bpwhelan95@gmail.com>
2024-02-04 00:48:33 -0500

    Revert "run prettier on root"
    This reverts commit 2491c88fb743a0b1511fd7658de3995c9a9f3908.

--- extension/src/pages/emby-page.ts
+++ extension/src/pages/emby-page.ts
@@ -17,45 +17,32 @@ document.addEventListener(
         }
         const deviceID = ApiClient?._deviceId;
         const apikey = ApiClient?._userAuthInfo.AccessToken;
-        await fetch('/Sessions?api_key=' + apikey + '&IsPlaying=True&DeviceId=' + deviceID)
-            .then((webResponse) => {
-                return webResponse.json();
-            })
-            .then((sessions) => {
-                var session = sessions[0];
-                var mediaID = session.PlayState.MediaSourceId;
-                var nowPlayingItem = session.NowPlayingItem;
-                response.basename = nowPlayingItem.FileName;
-                const subtitles: VideoDataSubtitleTrack[] = [];
-                nowPlayingItem.MediaStreams.filter(
-                    (stream: { IsTextSubtitleStream: any }) => stream.IsTextSubtitleStream
-                ).forEach((sub: { Codec: string; DisplayTitle: any; Language: any; Index: number }) => {
-                    var url =
-                        '/Videos/' +
-                        nowPlayingItem.Id +
-                        '/' +
-                        mediaID +
-                        '/Subtitles/' +
-                        sub.Index +
-                        '/Stream.' +
-                        sub.Codec +
-                        '?api_key=' +
-                        apikey;
-                    subtitles.push({
-                        label: sub.DisplayTitle,
-                        language: sub.Language,
-                        url: url,
-                        extension: sub.Codec,
-                    });
+        await fetch("/Sessions?api_key=" + apikey + "&IsPlaying=True&DeviceId=" + deviceID).then((webResponse) => {
+            return webResponse.json()
+        }).then((sessions) => {
+            var session = sessions[0];
+            var mediaID = session.PlayState.MediaSourceId;
+            var nowPlayingItem = session.NowPlayingItem;
+            response.basename = nowPlayingItem.FileName;
+            const subtitles: VideoDataSubtitleTrack[] = [];
+            nowPlayingItem.MediaStreams.filter((stream: { IsTextSubtitleStream: any; }) => stream.IsTextSubtitleStream).forEach((sub: { Codec: string; DisplayTitle: any; Language: any; Index: number }) => {
+                var url = "/Videos/"+ nowPlayingItem.Id + "/" + mediaID + "/Subtitles/" + sub.Index + "/Stream." + sub.Codec + "?api_key=" + apikey;
+                subtitles.push({
+                    label: sub.DisplayTitle,
+                    language: sub.Language,
+                    url: url,
+                    extension: sub.Codec,
                 });
-                response.subtitles = subtitles;
-
-                document.dispatchEvent(
-                    new CustomEvent('asbplayer-synced-data', {
-                        detail: response,
-                    })
-                );
             });
+            response.subtitles = subtitles;
+
+            document.dispatchEvent(
+                new CustomEvent('asbplayer-synced-data', {
+                    detail: response,
+                })
+            );
+        });
     },
     false
 );
+
--- scripts/merge-loc-from-en.js
+++ scripts/merge-loc-from-en.js
@@ -1,9 +1,9 @@
-const path = require('path');
-const fs = require('fs');
-const localesPath = path.join(__dirname, '../common/locales');
+const path = require('path')
+const fs = require('fs')
+const localesPath = path.join(__dirname, '../common/locales')
 
 const merge = (a, b) => {
-    const merged = { ...a, ...b };
+    const merged = {...a, ...b};
 
     for (const key of Object.keys(a)) {
         if (typeof a[key] !== 'object') {
@@ -11,7 +11,7 @@ const merge = (a, b) => {
         }
 
         if (key in b) {
-            merged[key] = merge(a[key], b[key]);
+            merged[key] = merge(a[key], b[key])
         }
     }
 
