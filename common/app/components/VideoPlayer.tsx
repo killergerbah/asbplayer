a8fa44c7ecf5bf34237fd84292aab8fff1983deb
Art Rizihs <artjoms.rizihs@gmail.com>
2024-05-11 12:55:53 +0100

    apply video step setting in the player

--- common/app/components/VideoPlayer.tsx
+++ common/app/components/VideoPlayer.tsx
@@ -765,9 +765,9 @@ export default function VideoPlayer({
                 }
 
                 if (increase) {
-                    updatePlaybackRate(Math.min(5, video.playbackRate + 0.1), true);
+                    updatePlaybackRate(Math.min(5, video.playbackRate + settings.speedChangeStep), true);
                 } else {
-                    updatePlaybackRate(Math.max(0.1, video.playbackRate - 0.1), true);
+                    updatePlaybackRate(Math.max(0.1, video.playbackRate - settings.speedChangeStep), true);
                 }
             },
             () => false
