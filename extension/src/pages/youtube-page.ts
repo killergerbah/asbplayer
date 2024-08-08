0dddbca1e589454ea8762ff73b767370e8654185
stu <stu@anvil.works>
2024-08-08 22:14:47 +0800

    revert speculative fix commit

--- extension/src/pages/youtube-page.ts
+++ extension/src/pages/youtube-page.ts
@@ -22,16 +22,7 @@ if (window.trustedTypes !== undefined) {
     trustedPolicy = window.trustedTypes.createPolicy('passThrough', {
         createHTML: (s: string) => s,
         createScript: (s: string) => s,
-        createScriptURL: (s: string) => s,
     });
-
-    if (window.trustedTypes.defaultPolicy === null) {
-        window.trustedTypes.createPolicy('default', {
-            createHTML: (s: string) => s,
-            createScript: (s: string) => s,
-            createScriptURL: (s: string) => s,
-        });
-    }
 }
 
 document.addEventListener(
