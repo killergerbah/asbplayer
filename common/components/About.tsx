a26c4d414ed14e184a3222ffa001f2349feedfba
R-J Lim <kgerbil@gmail.com>
2024-10-28 15:16:24 +0900

    Project links for all dependencies in about page

--- common/components/About.tsx
+++ common/components/About.tsx
@@ -41,7 +41,7 @@ const BorderedTableCell = withStyles((theme) => ({
 
 type Dependency = {
     name: string;
-    projectLink?: string;
+    projectLink: string;
     license: string;
     licenseLink: string;
     purpose: string;
@@ -51,66 +51,77 @@ type Dependency = {
 const dependencies: Dependency[] = [
     {
         name: 'react',
+        projectLink: 'https://react.dev',
         license: 'MIT',
         licenseLink: 'https://github.com/facebook/react/blob/v18.0.0/LICENSE',
         purpose: 'UI',
     },
     {
         name: 'Material UI',
+        projectLink: 'https://mui.com/material-ui',
         license: 'MIT',
         licenseLink: 'https://github.com/mui/material-ui/blob/v4.x/LICENSE',
         purpose: 'UI',
     },
     {
         name: 'Roboto',
+        projectLink: 'https://fonts.google.com/specimen/Roboto',
         license: 'Apache 2.0',
         licenseLink: 'https://fonts.google.com/specimen/Roboto/license',
         purpose: 'UI',
     },
     {
         name: 'Dexie.js',
+        projectLink: 'https://dexie.org',
         license: 'Apache 2.0',
         licenseLink: 'https://github.com/dexie/Dexie.js/blob/master/LICENSE',
         purpose: 'Persistence',
     },
     {
         name: 'flatten-interval-tree',
+        projectLink: 'https://github.com/alexbol99/flatten-interval-tree',
         license: 'MIT',
         licenseLink: 'https://github.com/alexbol99/flatten-interval-tree/blob/master/LICENSE',
         purpose: 'Subtitle rendering',
     },
     {
         name: 'srt-parser',
+        projectLink: 'https://github.com/qgustavor/srt-parser',
         license: 'MIT',
         licenseLink: 'https://github.com/qgustavor/srt-parser/blob/master/LICENSE',
         purpose: 'Subtitle parsing',
     },
     {
         name: 'ass-compiler',
+        projectLink: 'https://github.com/weizhenye/ass-compiler',
         license: 'MIT',
         licenseLink: 'https://github.com/weizhenye/ass-compiler/blob/master/LICENSE',
         purpose: 'Subtitle parsing',
     },
     {
         name: 'pgs-parser',
+        projectLink: 'https://github.com/killergerbah/pgs-parser',
         license: 'MIT',
         licenseLink: 'https://github.com/killergerbah/pgs-parser/blob/main/LICENSE.md',
         purpose: 'Subtitle parsing',
     },
     {
         name: 'fast-xml-parser',
+        projectLink: 'https://github.com/NaturalIntelligence/fast-xml-parser',
         license: 'MIT',
         licenseLink: 'https://github.com/NaturalIntelligence/fast-xml-parser/blob/master/LICENSE',
         purpose: 'Subtitle parsing',
     },
     {
         name: 'vtt.js',
+        projectLink: 'https://github.com/mozilla/vtt.js',
         license: 'Apache-2.0',
         licenseLink: 'https://github.com/mozilla/vtt.js/blob/master/LICENSE',
         purpose: 'Subtitle parsing',
     },
     {
         name: 'semver',
+        projectLink: 'https://github.com/npm/node-semver',
         license: 'ISC',
         licenseLink: 'https://github.com/npm/node-semver/blob/main/LICENSE',
         purpose: 'Version string parsing',
@@ -118,12 +129,14 @@ const dependencies: Dependency[] = [
 
     {
         name: 'hotkeys-js',
+        projectLink: 'https://github.com/jaywcjlove/hotkeys-js',
         license: 'MIT',
         licenseLink: 'https://github.com/jaywcjlove/hotkeys-js/blob/master/LICENSE',
         purpose: 'Keyboard shortcuts',
     },
     {
         name: 'i18next',
+        projectLink: 'https://www.i18next.com',
         license: 'MIT',
         licenseLink: 'https://github.com/i18next/i18next/blob/master/LICENSE',
         purpose: 'Localization',
@@ -137,24 +150,28 @@ const dependencies: Dependency[] = [
     },
     {
         name: 'sanitize-filename',
+        projectLink: 'https://github.com/parshap/node-sanitize-filename',
         license: 'ISC+WTFPL',
         licenseLink: 'https://github.com/parshap/node-sanitize-filename/blob/master/LICENSE.md',
         purpose: 'Filename sanitization',
     },
     {
         name: 'uuidjs',
+        projectLink: 'https://github.com/uuidjs/uuid',
         license: 'MIT',
         licenseLink: 'https://github.com/uuidjs/uuid/blob/main/LICENSE.md',
         purpose: 'UUID generation',
     },
     {
         name: 'url',
+        projectLink: 'https://github.com/defunctzombie/node-url',
         license: 'MIT',
         licenseLink: 'https://github.com/defunctzombie/node-url/blob/master/LICENSE',
         purpose: 'Polyfill',
     },
     {
         name: 'm3u8-parser',
+        projectLink: 'https://github.com/videojs/m3u8-parser',
         license: 'Apache-2.0',
         licenseLink: 'https://github.com/videojs/m3u8-parser/blob/main/LICENSE',
         purpose: 'Subtitle detection',
@@ -162,6 +179,7 @@ const dependencies: Dependency[] = [
     },
     {
         name: 'mpd-parser',
+        projectLink: 'https://github.com/videojs/mpd-parser',
         license: 'Apache-2.0',
         licenseLink: 'https://github.com/videojs/mpd-parser/blob/main/LICENSE',
         purpose: 'Subtitle detection',
