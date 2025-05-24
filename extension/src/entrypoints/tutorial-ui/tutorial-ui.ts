2b9786b9b30e9ec4e0971d0a41e798deda4c4724
R-J Lim <kgerbil@gmail.com>
2025-05-24 21:53:08 +0900

    Remove standalone tutorial page

--- extension/src/entrypoints/tutorial-ui/tutorial-ui.ts
+++
@@ -1,9 +0,0 @@
-import { renderTutorialUi } from '@/ui/tutorial';
-import '../video.content/video.css';
-import { currentPageDelegate } from '@/services/pages';
-
-window.addEventListener('load', () => {
-    currentPageDelegate()?.loadScripts();
-    const root = document.getElementById('root')!;
-    renderTutorialUi(root);
-});
