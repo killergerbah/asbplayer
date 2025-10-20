3470961179155cdd88a936830815b53c2f020acf
extremq <45830561+extremq@users.noreply.github.com>
2025-10-20 20:37:10 +0300

    Address pr comments for cijapanese.com

--- extension/src/entrypoints/cijapanese-page.ts
+++ extension/src/entrypoints/cijapanese-page.ts
@@ -1,15 +1,26 @@
 import { VideoData } from '@project/common';
-
 export default defineUnlistedScript(() => {
     document.addEventListener('asbplayer-get-synced-data', () => {
         const tracks = Array.from(document.querySelectorAll("video track"));
-        const subtitles = tracks.map((track, index)=>({
-            id: (index+1).toString(),
-            label: track.getAttribute("label"),
-            language: track.getAttribute("srclang"),
-            url: track.getAttribute("src"),
-            extension: track.getAttribute("src").split(".").at(-1),
-        }));
+        const subtitles = [];
+
+        for (let i = 0; i < tracks.length; i++) {
+            const track = tracks[i];
+            const url = track.getAttribute("src");
+            const label = track.getAttribute("label");
+            const language = track.getAttribute("srclang");
+            
+            if (!url || !label || !language) continue;
+
+            const extension = url.split(".").at(-1) || "";
+            subtitles.push({
+                id: (subtitles.length).toString(),
+                label,
+                language,
+                url,
+                extension,
+            });
+        }
 
         const response: VideoData = {
             error: "",
