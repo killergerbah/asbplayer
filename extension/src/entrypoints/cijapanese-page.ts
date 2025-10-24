a538aa41372858fb92cd1dfbb1a9169e1e35761f
R-J Lim <kgerbil@gmail.com>
2025-10-25 08:01:21 +0900

    Run prettier

--- extension/src/entrypoints/cijapanese-page.ts
+++ extension/src/entrypoints/cijapanese-page.ts
@@ -3,19 +3,19 @@ import { trackId } from '@/pages/util.ts';
 
 export default defineUnlistedScript(() => {
     document.addEventListener('asbplayer-get-synced-data', () => {
-        const tracks = Array.from(document.querySelectorAll("video track"));
-        const subtitles: VideoData["subtitles"] = [];
+        const tracks = Array.from(document.querySelectorAll('video track'));
+        const subtitles: VideoData['subtitles'] = [];
 
         for (const track of tracks) {
-            const url = track.getAttribute("src");
-            const label = track.getAttribute("label");
+            const url = track.getAttribute('src');
+            const label = track.getAttribute('label');
             if (url && label) {
                 const trackDef: VideoDataSubtitleTrackDef = {
                     label,
-                    language: track.getAttribute("srclang") ?? undefined,
+                    language: track.getAttribute('srclang') ?? undefined,
                     url,
-                    extension: url.split(".").at(-1)!,
-                }
+                    extension: url.split('.').at(-1)!,
+                };
                 subtitles.push({
                     id: trackId(trackDef),
                     ...trackDef,
@@ -24,8 +24,8 @@ export default defineUnlistedScript(() => {
         }
 
         const response: VideoData = {
-            error: "",
-            basename: document.title.replace(" | Comprehensible Japanese", ""),
+            error: '',
+            basename: document.title.replace(' | Comprehensible Japanese', ''),
             subtitles,
         };
         document.dispatchEvent(new CustomEvent('asbplayer-synced-data', { detail: response }));
