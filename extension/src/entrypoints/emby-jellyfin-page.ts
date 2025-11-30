9b522770fcfcc2be22fe8fa54b38a85c8763d13e
Hawkheart <hawkheart@hawkhe.art>
2025-11-30 05:28:20 -0800

    jellyfin: support non-default base URLs

--- extension/src/entrypoints/emby-jellyfin-page.ts
+++ extension/src/entrypoints/emby-jellyfin-page.ts
@@ -49,7 +49,7 @@ export default defineUnlistedScript(() => {
             ).forEach((sub: { Codec: string; DisplayTitle: any; Language: any; Index: number; Path: string }) => {
                 const extension = 'srt';
                 var url =
-                    window.location.origin +
+                    ApiClient._serverAddress +
                     '/Videos/' +
                     nowPlayingItem.Id +
                     '/' +
