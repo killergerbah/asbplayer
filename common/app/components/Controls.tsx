ed6d1572e9b59a6cdb327f5af4e72e9b45a21b16
ronzy <ninjakai25@gmail.com>
2026-01-06 20:26:06 -0600

    feature: add seekbar preview on hover

--- common/app/components/Controls.tsx
+++ common/app/components/Controls.tsx
@@ -272,13 +272,10 @@ function elementWidth(element: HTMLElement) {
 
 interface ProgressBarProps {
     onSeek: (progress: number) => void;
-    onSeekPreview: (progress: number) => string | null | undefined;
+    onSeekPreview: (progress: number) => string | undefined;
     value: number;
 }
 
-// right now this takes VideoPlayer's handle seek function
-// so what I could do is have parent pass down 
-// Thumbnail preview is part of controls, so its controls job to render it
 function ProgressBar({ onSeek, onSeekPreview, value }: ProgressBarProps) {
     const classes = useProgressBarStyles();
     const [mouseOver, setMouseOver] = useState(false);
@@ -316,8 +313,6 @@ function ProgressBar({ onSeek, onSeekPreview, value }: ProgressBarProps) {
         [onSeekPreview]
     );
 
-
-    // const handleMouseOver = useCallback(() => setMouseOver(true), []);
     const handleMouseOut = useCallback(() => setMouseOver(false), []);
     const progressWidth =
         Number.isFinite(value) && containerRef.current ? (elementWidth(containerRef.current) * value) / 100 : 0;
@@ -347,7 +342,6 @@ function ProgressBar({ onSeek, onSeekPreview, value }: ProgressBarProps) {
             <div
                 className={classes.mouseEventListener}
                 onClick={handleClick}
-                // onMouseOver={handleMouseOver}
                 onMouseMove={handleMouseOver}
                 onMouseOut={handleMouseOut}
             ></div>
@@ -529,7 +523,7 @@ interface ControlsProps {
     playbackRateEnabled?: boolean;
     onAudioTrackSelected: (id: string) => void;
     onSeek: (progress: number) => void;
-    onSeekPreview: (progress: number) => string | undefined | null;
+    onSeekPreview: (progress: number) => string | undefined;
     mousePositionRef: MutableRefObject<Point | undefined>;
     onShow?: (show: boolean) => void;
     onPause: () => void;
