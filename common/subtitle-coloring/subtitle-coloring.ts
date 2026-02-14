e801f3f9103b076efd0945acba39b6ecf23012f4
R-J Lim <kgerbil@gmail.com>
2026-02-14 20:24:30 +0900

    Fix some issues related to Netflix readings

--- common/subtitle-coloring/subtitle-coloring.ts
+++ common/subtitle-coloring/subtitle-coloring.ts
@@ -1271,11 +1271,11 @@ const LOGIC_ERROR_STYLE = `style="text-decoration: line-through red 3px double;"
 
 const applyTokenStyle = (fullText: string, token: Token, dt?: DictionaryTrack) => {
     const tokenText = applyReadingAnnotation(fullText, token, dt);
+    if (!dt?.dictionaryColorizeSubtitles) return tokenText;
     if (token.status === null) return `<span ${ERROR_STYLE}>${tokenText}</span>`;
     if (token.status === undefined) {
         return `<span ${LOGIC_ERROR_STYLE}>${tokenText}</span>`; // External tokens may flash this on initial load
     }
-    if (!dt?.dictionaryColorizeSubtitles) return tokenText;
 
     const s = HAS_LETTER_REGEX.test(tokenText)
         ? `<span class="${ASB_TOKEN_CLASS}${dt.dictionaryHighlightOnHover ? ` ${ASB_TOKEN_HIGHLIGHT_CLASS}` : ''}"`
