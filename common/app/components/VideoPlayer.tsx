d0fcfa089182fa389e9ae0c089917a1095d55a57
ronzy <ninjakai25@gmail.com>
2026-03-08 17:10:28 -0500

    remove testing component

--- common/app/components/VideoPlayer.tsx
+++ common/app/components/VideoPlayer.tsx
@@ -38,7 +38,6 @@ import {
     ensureStoragePersisted,
     subtitleTimestampWithDelay,
 } from '@project/common/util';
-import MemoryTester from '@project/common/components/MemoryTester';
 import { SubtitleCollection } from '@project/common/subtitle-collection';
 import { HoveredToken } from '@project/common/subtitle-coloring';
 import Clock from '../services/clock';
