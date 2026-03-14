11ead05dc941845a792e8c360cf1b77a74f6811c
mwojick <35327565+mwojick@users.noreply.github.com>
2026-03-14 00:04:51 -0700

    rm shims

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -39,34 +39,7 @@ export default defineConfig({
                     server.watcher.add(path.resolve(__dirname, '../common'));
                 },
             },
-            // {
-            //     name: 'vtt-js-esm-fix',
-            //     transform(code, id) {
-            //         if (/[/\\]vtt\.js[/\\]lib[/\\]/.test(id)) {
-            //             return { code: code.replace(/\}\(this\)\)/g, '}(globalThis))'), map: null };
-            //         }
-            //     },
-            // },
         ],
-        // resolve: {
-        //     alias: {
-        //         url: path.resolve(__dirname, 'src/url-shim.ts'),
-        //     },
-        // },
-        // optimizeDeps: {
-        //     rolldownOptions: {
-        //         plugins: [
-        //             {
-        //                 name: 'vtt-js-esm-fix',
-        //                 transform(code: string, id: string) {
-        //                     if (/[/\\]vtt\.js[/\\]lib[/\\]/.test(id)) {
-        //                         return { code: code.replace(/\}\(this\)\)/g, '}(globalThis))'), map: null };
-        //                     }
-        //                 },
-        //             },
-        //         ],
-        //     },
-        // },
     }),
     zip: {
         sourcesRoot: '..',
