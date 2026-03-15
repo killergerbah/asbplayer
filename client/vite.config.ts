ef16cd895b8a45b214b72590011c7fc4b17d356c
Matt Wojick <35327565+mwojick@users.noreply.github.com>
2026-03-15 05:09:44 -0700

    Dev/vite8 (#940)
    * try vite8
    
    * try url shim
    
    * update broken packages
    
    * rm shims
    
    * fix client config
    
    * add refs to videojs-vtt.js
    
    * rm unused client config
    
    * update wxt + add @types/videojs-vtt.js

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig, loadEnv, normalizePath } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import { VitePWA } from 'vite-plugin-pwa';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
@@ -11,9 +10,11 @@ export default defineConfig(({ mode }) => {
     const base = env.VITE_APP_BASE_PATH || '/asbplayer';
     return {
         base,
+        resolve: {
+            tsconfigPaths: true,
+        },
         plugins: [
             react(),
-            viteTsconfigPaths(),
             createHtmlPlugin({
                 inject: {
                     data: {
